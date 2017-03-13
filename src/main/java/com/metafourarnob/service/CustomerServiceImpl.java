16a24209beeb78de52479f2611e188c64d33cefe
farhan-arnob-m4 <farhanarnob@metafour.com>
2017-03-13 17:46:22 +0600

    have been Omitted unnecessary setter method in CustomerServiceImpl.

--- src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
+++ src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
@@ -24,10 +24,10 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 	
 //	@Autowired
-	public void setCustomerRepository(CustomerRepository customerRepository) {
-		System.out.println("We are using setter injection");
-		this.customerRepository = customerRepository;
-	}
+//	public void setCustomerRepository(CustomerRepository customerRepository) {
+//		System.out.println("We are using setter injection");
+//		this.customerRepository = customerRepository;
+//	}
 
 
 

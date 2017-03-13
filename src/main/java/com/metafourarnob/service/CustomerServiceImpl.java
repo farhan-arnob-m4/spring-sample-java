33320b97076c22330560546e814fb07bee7cb43e
farhan-arnob-m4 <farhanarnob@metafour.com>
2017-03-13 17:50:03 +0600

    In java configuration: setter based injection in autowired annotation has been implemented.

--- src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
+++ src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
@@ -9,7 +9,7 @@ import com.metafourarnob.repository.CustomerRepository;
 
 public class CustomerServiceImpl implements CustomerService {
 	
-	@Autowired
+//	@Autowired
 	private CustomerRepository customerRepository;
 	
 	
@@ -23,11 +23,11 @@ public class CustomerServiceImpl implements CustomerService {
 		this.customerRepository = customerRepository;
 	}
 	
-//	@Autowired
-//	public void setCustomerRepository(CustomerRepository customerRepository) {
-//		System.out.println("We are using setter injection");
-//		this.customerRepository = customerRepository;
-//	}
+	@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("We are using setter injection");
+		this.customerRepository = customerRepository;
+	}
 
 
 
